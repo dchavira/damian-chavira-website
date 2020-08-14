941d0f64061f332210f090c5b2b6b0810a8c5207
dchavira <37552297+dchavira@users.noreply.github.com>
2020-08-14 10:45:39 -0700

    Added some recent 3d art

--- src/components/projects/art/art.js
+++ src/components/projects/art/art.js
@@ -8,20 +8,26 @@ class Art extends React.Component {
                 <h1 className='title'>3D Art</h1>
                 <div id='art'>
                 <ul className='artContainer'>
-                    <li className='thumb'>
-                        <a href='/img/3d/sunset.jpg'><img className='r1' src='/img/3d/sunset.jpg' alt="Jazz Chords Home Screen"/></a>
+                <li className='thumb'>
+                        <a href='/img/3d/DeerForest.png'><img className='r1' src='/img/3d/DeerForest.png' alt="DeerForest"/></a>
                     </li>
                     <li className='thumb'>
-                        <a href='/img/3d/balanceVR.jpeg'><img className='r1' src='/img/3d/balanceVR.jpeg' alt="Jazz Chords Home Screen" /></a>
+                        <a href='/img/3d/City.png'><img className='r1' src='/img/3d/City.png' alt="City"/></a>
                     </li>
                     <li className='thumb'>
-                        <a href='/img/3d/album finished.jpg'><img className='r1' src='/img/3d/album finished.jpg' alt="Jazz Chords Home Screen"/></a>
+                        <a href='/img/3d/sunset.jpg'><img className='r1' src='/img/3d/sunset.jpg' alt="Sunset"/></a>
                     </li>
                     <li className='thumb'>
-                        <a href='/img/3d/image1.jpg'><img className='r1' src='/img/3d/image1.jpg' alt="Jazz Chords Home Screen"/></a>
+                        <a href='/img/3d/balanceVR.jpeg'><img className='r1' src='/img/3d/balanceVR.jpeg' alt="BalanceVR" /></a>
                     </li>
                     <li className='thumb'>
-                        <a href='/img/3d/flowers.jpg'><img className='r1' src='/img/3d/flowers.jpg' alt="Jazz Chords Home Screen"/></a>
+                        <a href='/img/3d/album finished.jpg'><img className='r1' src='/img/3d/album finished.jpg' alt="Album"/></a>
+                    </li>
+                    <li className='thumb'>
+                        <a href='/img/3d/image1.jpg'><img className='r1' src='/img/3d/image1.jpg' alt="Fern"/></a>
+                    </li>
+                    <li className='thumb'>
+                        <a href='/img/3d/flowers.jpg'><img className='r1' src='/img/3d/flowers.jpg' alt="Flowers"/></a>
                     </li>
                 </ul>
             </div>
